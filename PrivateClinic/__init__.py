ba91192c77a8d71139b237ebf040efcf2963db1c
nguyenvankiet2511 <nguyenthikimlyts99@gmail.com>
2023-12-28 14:37:31 +0700

    kiet

--- PrivateClinic/__init__.py
+++ PrivateClinic/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
 from urllib.parse import quote
 
 app = Flask(__name__)
+app.secret_key="sacfasfgwgwgwgwgwegehehehehru5hrt"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/privateclinicdb?charset=utf8mb4" % quote(
     "Admin@123")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app=app)
+admin= Admin(app=app, name='ADMIN', template_mode='bootstrap4')
